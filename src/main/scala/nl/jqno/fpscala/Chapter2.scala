7a6048f92b86c83446b30e1480ef30aa253422c7
Jan Ouwens <jan.ouwens@gmail.com>
2015-09-03 21:17:07 +0200

    Fibonacci counting up vs counting down

--- src/main/scala/nl/jqno/fpscala/Chapter2.scala
+++ src/main/scala/nl/jqno/fpscala/Chapter2.scala
@@ -4,17 +4,27 @@ import scala.annotation.tailrec
 
 object Chapter2 extends App {
   // 2.1: Fibonacci
+//  def fib(n: Int): Int = {
+//    @tailrec
+//    def go(n: Int, prev: Int, acc: Int): Int =
+//      if (n == 0) prev
+//      else if (n <= 1) acc
+//      else go(n - 1, acc, prev + acc)
+//
+//    if (n < 0) throw new IllegalArgumentException(s"$n < 0")
+//    else go(n, 0, 1)
+//  }
+
   def fib(n: Int): Int = {
     @tailrec
-    def go(n: Int, prev: Int, acc: Int): Int =
-      if (n == 0) prev
-      else if (n <= 1) acc
-      else go(n - 1, acc, prev + acc)
-    
+    def go(c: Int, acc: Int, curr: Int): Int =
+      if (c == n) acc
+      else go(c + 1, curr, acc + curr)
+
     if (n < 0) throw new IllegalArgumentException(s"$n < 0")
-    else go(n, 0, 1)
+    else go(0, 0, 1)
   }
-  
+
   
   // 2.2: isSorted
   def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
