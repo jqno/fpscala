2b54110f731968ca456c6293691664d9753e96f7
Jan Ouwens <jan.ouwens@gmail.com>
2016-08-31 17:26:20 +0200

    Exercise 4.1: Option without the matches

--- src/main/scala/nl/jqno/fpscala/Chapter4Option.scala
+++ src/main/scala/nl/jqno/fpscala/Chapter4Option.scala
@@ -5,22 +5,20 @@ sealed trait Option[+A] {
     case Some(value) => Some(f(value))
     case None => None
   }
-  def flatMap[B](f: A => Option[B]): Option[B] = this match {
-    case Some(value) => f(value)
-    case None => None
-  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
+
   def getOrElse[B >: A](default: => B): B = this match {
     case Some(value) => value
     case None => default
   }
-  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
-    case Some(_) => this
-    case None => ob
-  }
-  def filter(f: A => Boolean): Option[A] = this match {
-    case Some(value) if f(value) => this
-    case _ => None
-  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(value => if (f(value)) Some(value) else None)
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
