5546c1244ee64ae6639a41d3e2c439ad4ae210fd
Jan Ouwens <jan.ouwens@gmail.com>
2016-08-31 08:04:14 +0200

    Exercise 4.1: Option

--- src/main/scala/nl/jqno/fpscala/Chapter4Option.scala
+++ src/main/scala/nl/jqno/fpscala/Chapter4Option.scala
@@ -1,5 +1,26 @@
 package nl.jqno.fpscala
 
-sealed trait Option[+A]
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(value) => Some(f(value))
+    case None => None
+  }
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(value) => f(value)
+    case None => None
+  }
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(value) => value
+    case None => default
+  }
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case Some(_) => this
+    case None => ob
+  }
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(value) if f(value) => this
+    case _ => None
+  }
+}
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
--- src/test/scala/nl/jqno/fpscala/Chapter4OptionTest.scala
+++ src/test/scala/nl/jqno/fpscala/Chapter4OptionTest.scala
@@ -3,5 +3,65 @@ package nl.jqno.fpscala
 import org.scalatest.{Matchers, FlatSpec}
 
 class Chapter4OptionTest extends FlatSpec with Matchers {
+  val some: Option[Int] = Some(1)
+  val some2: Option[Int] = Some(2)
+  val none: Option[Int] = None
+  val even = (i: Int) => i % 2 == 0
+  val evenOpt = (i: Int) => if (even(i)) Some(i) else None
 
+
+  behavior of "map"
+
+  it should "map a Some" in {
+    some.map(_ + 1) should be (Some(2))
+  }
+
+  it should "map a None" in {
+    none.map(_ + 1) should be (None)
+  }
+
+
+  behavior of "flatMap"
+
+  it should "flatMap a Some" in {
+    some2.flatMap(evenOpt) should be (Some(2))
+    some.flatMap(evenOpt) should be (None)
+  }
+
+  it should "flatMap a None" in {
+    none.flatMap(evenOpt) should be (None)
+  }
+
+
+  behavior of "getOrElse"
+
+  it should "return the value if it's a Some" in {
+    some.getOrElse(42) should be (1)
+  }
+
+  it should "return the default value if it's a None" in {
+    none.getOrElse(42) should be (42)
+  }
+
+
+  behavior of "orElse"
+
+  it should "return itself if it's a Some" in {
+    some.orElse(some2) should be (some)
+  }
+
+  it should "return the default value if it's a None" in {
+    none.orElse(some2) should be (some2)
+  }
+
+
+  behavior of "filter"
+
+  it should "return itself if the predicate is true" in {
+    some2.filter(even) should be (some2)
+  }
+
+  it should "return None if the predicate is false" in {
+    some.filter(even) should be (None)
+  }
 }
