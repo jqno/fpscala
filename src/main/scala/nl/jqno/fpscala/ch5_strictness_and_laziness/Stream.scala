f478f6df8a40ebb3190530a1ecea74a3f55c3c06
Jan Ouwens <jan.ouwens@gmail.com>
2016-09-05 07:45:02 +0200

    Chapter 5: initial

---
+++ src/main/scala/nl/jqno/fpscala/ch5_strictness_and_laziness/Stream.scala
@@ -0,0 +1,18 @@
+package nl.jqno.fpscala.ch5_strictness_and_laziness
+
+sealed trait Stream[+A]
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](h: => A, t: => Stream[A]): Stream[A] = {
+    lazy val head = h
+    lazy val tail = t
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
---
+++ src/test/scala/nl/jqno/fpscala/ch5_strictness_and_laziness/StreamTest.scala
@@ -0,0 +1,7 @@
+package nl.jqno.fpscala.ch5_strictness_and_laziness
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class StreamTest extends FlatSpec with Matchers {
+
+}
