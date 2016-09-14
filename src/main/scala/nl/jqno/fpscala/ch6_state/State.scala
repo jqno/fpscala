052e0a5bcc31dd45efd9169514364ffc8d2676c4
Jan Ouwens <jan.ouwens@gmail.com>
2016-09-14 07:46:27 +0200

    Chapter 6: modify method from the book

--- src/main/scala/nl/jqno/fpscala/ch6_state/State.scala
+++ src/main/scala/nl/jqno/fpscala/ch6_state/State.scala
@@ -23,4 +23,13 @@ object State {
 
   def unit[S, A](a: A): State[S, A] =
     State(s => (a, s))
+
+
+  // Taken from the book
+  def get[S]: State[S, S] = State(s => (s, s))
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
 }
