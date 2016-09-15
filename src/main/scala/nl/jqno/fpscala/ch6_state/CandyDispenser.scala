f82f4ba7efce6fe06ed71c368665a189efffb51d
Jan Ouwens <jan.ouwens@gmail.com>
2016-09-15 07:53:49 +0200

    Exercise 6.12: simplifies signature

--- src/main/scala/nl/jqno/fpscala/ch6_state/CandyDispenser.scala
+++ src/main/scala/nl/jqno/fpscala/ch6_state/CandyDispenser.scala
@@ -8,12 +8,12 @@ case class Machine(locked: Boolean, coins: Int, candies: Int)
 
 object CandyDispenser {
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = State { machine =>
-    val initial = ((machine.coins, machine.candies), machine)
-    inputs.foldLeft(initial)((acc, curr) => simulateSingleInput(curr).run(acc._2))
+    val next = inputs.foldLeft(machine)((acc, curr) => simulateSingleInput(curr)(acc))
+    ((next.coins, next.candies), next)
   }
 
-  private def simulateSingleInput(input: Input): State[Machine, (Int, Int)] = State { machine =>
-    val s = input match {
+  private def simulateSingleInput(input: Input): Machine => Machine = machine => {
+    input match {
       case Coin if machine.locked && machine.candies > 0 =>
         machine.copy(locked = false, coins = machine.coins + 1)
       case Turn if !machine.locked && machine.candies > 0 =>
@@ -21,6 +21,5 @@ object CandyDispenser {
       case _ =>
         machine
     }
-    ((s.coins, s.candies), s)
   }
 }
