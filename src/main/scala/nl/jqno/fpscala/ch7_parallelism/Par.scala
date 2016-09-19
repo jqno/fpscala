58cf410189f15443f9546075d6e3237d85d0f3a3
Jan Ouwens <jan.ouwens@gmail.com>
2016-09-19 17:03:42 +0200

    Chapter 7: initial

---
+++ src/main/scala/nl/jqno/fpscala/ch7_parallelism/Par.scala
@@ -0,0 +1,26 @@
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    override def isDone = true
+    override def get(timeout: Long, units: TimeUnit) = get
+    override def isCancelled = false
+    override def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      override def call = a(es).get
+    })
+}
---
+++ src/test/scala/nl/jqno/fpscala/ch7_parallelism/ParTest.scala
@@ -0,0 +1,4 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class ParTest extends FlatSpec with Matchers {
+}
