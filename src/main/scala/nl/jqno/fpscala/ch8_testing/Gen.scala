a124278da36e45bc715a4a0906d6ddd1ec944c35
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-07 07:46:16 +0100

    Chapter 8: fixes compiler errors

--- src/main/scala/nl/jqno/fpscala/ch8_testing/Gen.scala
+++ src/main/scala/nl/jqno/fpscala/ch8_testing/Gen.scala
@@ -25,8 +25,8 @@ object Gen {
 }
 
 trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+  def map[B](f: A => B): Gen[B] = ???
+  def flatMap[B](f: A => Gen[B]): Gen[B] = ???
 }
 
 trait SGen[+A] {
