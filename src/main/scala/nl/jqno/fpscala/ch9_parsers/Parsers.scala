5012f4b14bbfad5ccb1ee2eb8f64f08ce9444f35
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-20 07:55:19 +0100

    Chapter 9: adds unimplemented methods

--- src/main/scala/nl/jqno/fpscala/ch9_parsers/Parsers.scala
+++ src/main/scala/nl/jqno/fpscala/ch9_parsers/Parsers.scala
@@ -1,12 +1,35 @@
 package fpinscala.parsing
 
 import language.higherKinds
+import language.implicitConversions
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+  def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] =
+    ParserOps(f(a))
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+  def many[A](p: Parser[A]): Parser[List[A]]
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+  def slice[A](p: Parser[A]): Parser[String]
+  def many1[A](p: Parser[A]): Parser[List[A]]
+  def product[A, B](p1: Parser[A], p2: Parser[B]): Parser[(A, B)]
+
+
   case class ParserOps[A](p: Parser[A]) {
-
-
+    def | [B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    def many: Parser[List[A]] = self.many(p)
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def ** [B](p2: => Parser[B]): Parser[(A, B)] = self.product(p, p2)
+    def product[B](p2: => Parser[B]): Parser[(A, B)] = self.product(p, p2)
   }
 
   object Laws {
