6af322444ae6f29646f1acf88b5ba9fdaf3b7470
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-20 16:46:40 +0100

    Exercise 9.1: map2 and many1

--- src/main/scala/nl/jqno/fpscala/ch9_parsers/Parsers.scala
+++ src/main/scala/nl/jqno/fpscala/ch9_parsers/Parsers.scala
@@ -7,6 +7,16 @@ import nl.jqno.fpscala.ch8_testing.Prop._
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
+  // Exercise 9.1: map2 and many1
+  def map2[A, B, C](p1: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] =
+    product(p1, p2) map (t => f(t._1, t._2))
+
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _)
+
+
+
+
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
   def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
   def succeed[A](a: A): Parser[A] = string("") map (_ => a)
@@ -21,7 +31,6 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   def many[A](p: Parser[A]): Parser[List[A]]
   def map[A, B](p: Parser[A])(f: A => B): Parser[B]
   def slice[A](p: Parser[A]): Parser[String]
-  def many1[A](p: Parser[A]): Parser[List[A]]
   def product[A, B](p1: Parser[A], p2: Parser[B]): Parser[(A, B)]
 
 
