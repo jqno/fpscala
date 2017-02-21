aab76d578a7cc24ab13ec0496fc24814db7fb969
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-21 07:47:54 +0100

    Exercise 9.8: map in terms of flatMap and other combinators

--- src/main/scala/nl/jqno/fpscala/ch9_parsers/Parsers.scala
+++ src/main/scala/nl/jqno/fpscala/ch9_parsers/Parsers.scala
@@ -41,6 +41,10 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
     p1.flatMap(a => p2.flatMap(b => succeed(f(a, b))))
 
 
+  // Exercise 9.8: map in terms of flatMap and other combinators
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] =
+    p.flatMap(a => succeed(f(a)))
+
 
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
   def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
@@ -52,7 +56,6 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] =
     ParserOps(f(a))
 
-  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
   def slice[A](p: Parser[A]): Parser[String]
   def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
