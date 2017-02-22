57fb18a699888548fdd525c5f17d92e0d2239c2d
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-22 16:55:19 +0100

    Exercise 9.9: parsers for all except JObject

--- src/main/scala/nl/jqno/fpscala/ch9_parsers/JSON.scala
+++ src/main/scala/nl/jqno/fpscala/ch9_parsers/JSON.scala
@@ -15,6 +15,36 @@ object JSON {
   def jsonParser[Err, Parser[+_]](P: Parsers[Err, Parser]): Parser[JSON] = {
     import P._
     val spaces = char(' ').many.slice
+    val comma = (spaces ** char(',') ** spaces) map (t => t._1._1 + t._1._2 + t._2)
+
+    val jnull: Parser[JSON] =
+      string("null") map (_ => JNull)
+    val jnumber: Parser[JSON] =
+      regex("\\d+.\\d+".r) map (n => JNumber(n.toDouble))
+    val jstring: Parser[JSON] =
+      for {
+        _ <- char('"')
+        s <- regex("[^\"]*".r)
+        _ <- char('"')
+      } yield JString(s)
+    val jbool: Parser[JSON] =
+      regex("true|false".r) map (b => if (b == "true") JBool(true) else JBool(false))
+    lazy val innerJarray: Parser[JArray] = // incorrect: still allows trailing comma's. But I don't care yet ;)
+      (for {
+        j <- jsonParser(P)
+        _ <- comma
+        js <- innerJarray
+      } yield JArray(js.get :+ j)) | succeed(JArray(IndexedSeq.empty[JSON]))
+    val jarray: Parser[JSON] =
+      for {
+        _ <- char('[')
+        _ <- spaces
+        a <- innerJarray
+        _ <- spaces
+        _ <- char(']')
+      } yield a
+
+
     ???
   }
 }
