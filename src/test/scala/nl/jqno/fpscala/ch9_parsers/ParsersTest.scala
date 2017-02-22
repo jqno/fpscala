5ed6b4e1c2c005081af3ea987abe3e8fea98ac7e
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-22 07:28:13 +0100

    Chapter 9: no tests today

--- src/test/scala/nl/jqno/fpscala/ch9_parsers/ParsersTest.scala
+++ src/test/scala/nl/jqno/fpscala/ch9_parsers/ParsersTest.scala
@@ -3,5 +3,7 @@ package nl.jqno.fpscala.ch8_testing
 import org.scalatest.{FlatSpec, Matchers}
 
 class ParsersTest extends FlatSpec with Matchers {
+  // Didn't realise before: we won't have anything executable until near the end of the chapter.
+  // Hence: no tests today.
 }
 
