a08ca7512cdeb89ca7ad2c8284d1e875598f35e7
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-23 07:55:38 +0100

    Exercise 9.9: parser for JObject

--- src/main/scala/nl/jqno/fpscala/ch9_parsers/JSON.scala
+++ src/main/scala/nl/jqno/fpscala/ch9_parsers/JSON.scala
@@ -17,17 +17,17 @@ object JSON {
     val spaces = char(' ').many.slice
     val comma = (spaces ** char(',') ** spaces) map (t => t._1._1 + t._1._2 + t._2)
 
-    val jnull: Parser[JSON] =
+    val jnull: Parser[JNull.type] =
       string("null") map (_ => JNull)
-    val jnumber: Parser[JSON] =
+    val jnumber: Parser[JNumber] =
       regex("\\d+.\\d+".r) map (n => JNumber(n.toDouble))
-    val jstring: Parser[JSON] =
+    val jstring: Parser[JString] =
       for {
         _ <- char('"')
         s <- regex("[^\"]*".r)
         _ <- char('"')
       } yield JString(s)
-    val jbool: Parser[JSON] =
+    val jbool: Parser[JBool] =
       regex("true|false".r) map (b => if (b == "true") JBool(true) else JBool(false))
     lazy val innerJarray: Parser[JArray] = // incorrect: still allows trailing comma's. But I don't care yet ;)
       (for {
@@ -35,7 +35,7 @@ object JSON {
         _ <- comma
         js <- innerJarray
       } yield JArray(js.get :+ j)) | succeed(JArray(IndexedSeq.empty[JSON]))
-    val jarray: Parser[JSON] =
+    val jarray: Parser[JArray] =
       for {
         _ <- char('[')
         _ <- spaces
@@ -43,8 +43,31 @@ object JSON {
         _ <- spaces
         _ <- char(']')
       } yield a
+    val entryJobject: Parser[(String, JSON)] =
+      for {
+        k <- jstring
+        _ <- spaces
+        _ <- char('=')
+        _ <- spaces
+        v <- jsonParser(P)
+      } yield (k.get, v)
+    lazy val entriesJobject: Parser[JObject] =
+      (for {
+        _ <- spaces
+        e <- entryJobject
+        _ <- spaces
+        _ <- char(',')
+        es <- entriesJobject
+      } yield JObject(es.get + e)) | succeed(JObject(Map.empty))
+    val jobject: Parser[JObject] =
+      for {
+        _ <- char('{')
+        es <- entriesJobject
+        _ <- spaces
+        _ <- char('}')
+      } yield es
 
 
-    ???
+    jobject
   }
 }
