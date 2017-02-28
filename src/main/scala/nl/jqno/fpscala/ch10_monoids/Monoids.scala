55dc4c9444ef71a800199e457fe70bae9a2a2098
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-28 07:41:05 +0100

    Exercise 10.1: monoids for Int and Boolean

--- src/main/scala/nl/jqno/fpscala/ch10_monoids/Monoids.scala
+++ src/main/scala/nl/jqno/fpscala/ch10_monoids/Monoids.scala
@@ -15,4 +15,26 @@ object Monoids extends App {
     def op(a1: List[A], a2: List[A]) = a1 ++ a2
     val zero = List.empty[A]
   }
+
+
+  // Exercise 10.1: monoids for Int and Boolean
+  val intAddition = new Monoid[Int] {
+    def op(a1: Int, a2: Int) = a1 + a2
+    val zero = 0
+  }
+
+  val intMultiplication = new Monoid[Int] {
+    def op(a1: Int, a2: Int) = a1 * a2
+    val zero = 1
+  }
+
+  val booleanOr = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean) = a1 || a2
+    val zero = false
+  }
+
+  val booleanAnd = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean) = a1 && a2
+    val zero = true
+  }
 }
